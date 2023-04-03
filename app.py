f9da0b93b5b96a5bb31b82b32c080592ea028e7c
kagwicharles <kagwitheuri@gmail.com>
2023-04-03 09:49:31 +0300

    dependency change

--- app.py
+++ app.py
@@ -114,9 +114,9 @@ def upload():
     return response
 
 
-# this section is used by gunicorn to serve the app on Heroku
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 # if __name__ == '__main__':  
 #     app.run(host='0.0.0.0', port=80)
     # uncomment this section to serve the app locally with gevent at:  http://localhost:5000
